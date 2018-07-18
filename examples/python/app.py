08b9ced0392c0b698e2f9a248f0fa514464a3630
Luis Pabón <luis@portworx.com>
2018-07-17 22:12:34 -0400

    python tutorial

--- examples/python/app.py
+++ examples/python/app.py
@@ -8,18 +8,27 @@ try:
     # Cluster connection
     clusters = api_pb2_grpc.OpenStorageClusterStub(channel)
     ic_resp = clusters.InspectCurrent(api_pb2.SdkClusterInspectCurrentRequest())
-    print('Conntected to {0} with status {1}'.format(ic_resp.cluster.id, api_pb2.Status.Name(ic_resp.cluster.status)))
+    print('Conntected to {0} with status {1}'.format(
+        ic_resp.cluster.id,
+        api_pb2.Status.Name(ic_resp.cluster.status)
+    ))
 
     # Create a volume
     volumes = api_pb2_grpc.OpenStorageVolumeStub(channel)
     v_resp = volumes.Create(api_pb2.SdkVolumeCreateRequest(
         name="myvol",
         spec=api_pb2.VolumeSpec(
-            size=10*1024*1024*1024,
+            size=100*1024*1024*1024,
             ha_level=3,
         )
     ))
     print('Volume id is {0}'.format(v_resp.volume_id))
+
+    # Create a snapshot
+    snap = volumes.SnapshotCreate(api_pb2.SdkVolumeSnapshotCreateRequest(
+        volume_id=v_resp.volume_id,
+    ))
+    print('Snapshot created with id {0}'.format(snap.snapshot_id))
 
     # Create a credentials
     creds = api_pb2_grpc.OpenStorageCredentialsStub(channel)
@@ -46,6 +55,20 @@ try:
         volume_id=v_resp.volume_id
     ))
     backup_status = status_resp.statuses[v_resp.volume_id]
-    print('Status of the backup is {0}'.format(api_pb2.SdkCloudBackupStatusType.Name(backup_status.status)))
+    print('Status of the backup is {0}'.format(
+        api_pb2.SdkCloudBackupStatusType.Name(backup_status.status)
+    ))
+
+    # Show history
+    history = backups.History(api_pb2.SdkCloudBackupHistoryRequest(
+        src_volume_id=v_resp.volume_id
+    ))
+    print('Backup history for volume {0}'.format(v_resp.volume_id))
+    for item in history.history_list:
+        print('Time:{0} Status:{1}'.format(
+            item.timestamp.ToJsonString(),
+            api_pb2.SdkCloudBackupStatusType.Name(item.status)
+        ))
+
 except grpc.RpcError as e:
     print('Failed: code={0} msg={1}'.format(e.code(), e.details()))
